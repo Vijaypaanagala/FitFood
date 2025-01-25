816079c3a6138328c61d2765ed0f19d2e86f5518
Vijaypaanagala <paanagalavijaykumar@gmail.com>
2025-01-25 23:27:28 +0530

    chnages

--- frontend/src/pages/Updatefood.jsx
+++ frontend/src/pages/Updatefood.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
-import '../styles/updateFood.css'; // Import the CSS file
+import '../styles/UpdateFood.css'; // Import the CSS file
 
 function Updatefood() {
   const [title, setTitle] = useState('');
